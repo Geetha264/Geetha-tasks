6b613f22f9706ccd6f43d77327b9a255b07ff825
Admin <Admin@DESKTOP-BKD4TVR>
2025-07-25 18:11:32 +0530

    test

---
+++ tasks/src/tasks2/task12.java
@@ -0,0 +1,23 @@
+package tasks2;
+
+
+import java.util.*;
+
+public class task12 {
+ public static void main(String[] args) {
+ List<String> names = Arrays.asList("Alice", "Bob", "Anil", "Kiran",
+"Amit");
+ // Print all names
+ names.forEach(name -> System.out.println("Name: " + name));
+ // Filter names starting with "A"
+ System.out.println("\nNames starting with A:");
+ names.stream()
+ .filter(n -> n.startsWith("A"))
+ .forEach(System.out::println);
+ // Sort names
+ System.out.println("\nSorted Names:");
+ names.stream()
+ .sorted()
+ .forEach(System.out::println);
+ }
+}
